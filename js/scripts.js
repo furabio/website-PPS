5b776758ad712853b919e775cc33eba8ddccd9e4
Furabio <flvdeveloper@gmail.com>
2016-11-19 16:36:19 -0200

    website finish

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('.menu-hamburguer').on('click', function() {
-        var menu = $('nav.header-navegation, .header-menu li');
+        var menu = $('nav.header-navegation');
         if (menu.hasClass('ativo')) {
             menu.removeClass('ativo');
         }else {
